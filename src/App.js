935bdbee6002116dd07abc3d8de15ef78efffc24
ayushguptasah <a4ayush2gupta@gmail.com>
2022-03-22 16:34:53 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import React, { Component } from "react";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [Mode, setMode] = useState("light"); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-        setAlert(null);
-      }, 1500);
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-  const toggleMode = () =>{
-    if (Mode === 'light') {
-      setMode('dark')
-      document.body.style.backgroundColor = '#042743'
-      showAlert("Dark mode has been enabled", "success")
-    }
-    else {
-      setMode('light')
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success")
+  const toggleMode = () => {
+    if (Mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
   };
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
       {/* <Navbar/> */}
+
       <Navbar title="TextUtils" mode={Mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter The Text to Analyze Below:" mode={Mode} />
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter The Text to Analyze Below:"
+          mode={Mode}
+        />
+        {/* <About/> */}
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -77,14 +77,22 @@ export default function Navbar(props) {
               Search
             </button>
           </form> */}
-          <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
             <input
-              className="form-check-input" onClick={props.toggleMode}
+              className="form-check-input"
+              onClick={props.toggleMode}
               type="checkbox"
               role="switch"
               id="flexSwitchCheckDefault"
             />
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
               Enable DarkMode
             </label>
           </div>
